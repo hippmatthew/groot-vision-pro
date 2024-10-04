07de53d20871c67b05f3d91912dcb7ae2442c429
Matthew Hipp <74476804+hippmatthew@users.noreply.github.com>
2024-10-04 01:55:08 -0400

    WIP

--- src/gvp_engine/engine.rs
+++ src/gvp_engine/engine.rs
@@ -1,9 +1,15 @@
 use ash::vk;
 
-macro_rules! c_str() {
-  ($s:expr) => {
+use std::{ffi::CStr, vec::Vec};
 
-  }
+macro_rules! c_str {
+  ($s:expr) => {
+    unsafe { CStr::from_bytes_with_nul_unchecked(format!("{}\0", $s).as_bytes()) }
+  };
+}
+
+macro_rules! gvp_version {
+  () => { vk::make_api_version(0, 0, 3, 0) };
 }
 
 pub struct GVPEngine {
@@ -13,7 +19,12 @@ pub struct GVPEngine {
 
 impl GVPEngine {
   pub fn init() -> Self {
-    let (entry, instance) = GVPEngine::create_instance();
+    let entry = match unsafe { ash::Entry::load() } {
+      Ok(entry) => entry,
+      Err(error) => panic!("failed to load vulkan with error: {error}")
+    };
+
+    let instance = GVPEngine::create_instance(&entry);
 
     GVPEngine {
       entry,
@@ -21,9 +32,28 @@ impl GVPEngine {
     }
   }
 
-  fn create_instance() -> (ash::Entry, ash::Instance) {
+  fn create_instance(entry: &ash::Entry) -> ash::Instance {
     let application_info = {
       vk::ApplicationInfo::default()
+        .application_name(c_str!("Groot Vision Pro"))
+        .application_version(gvp_version!())
+        .engine_name(c_str!("GVP Engine"))
+        .engine_version(gvp_version!())
+        .api_version(vk::API_VERSION_1_3)
+    };
+
+    let layers = [ c_str!("VK_KHR_validation").as_ptr() ];
+    let extensions = Vec::<*const i8>::new();
+
+    let properties = match unsafe { entry.enumerate_instance_extension_properties(None) } {
+      Ok(properties) => properties,
+      Err(error) => panic!("failed to get instance extension properties with error: {error}")
+    };
+
+    for property in properties {
+      if property.extension_name. {
+        continue;
+      }
     };
   }
 }
